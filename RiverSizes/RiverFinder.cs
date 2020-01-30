f11b00a7fbd66cc97515d7137374a367974b2c4b
Christian Nytra <christian.nytra@co-orga.de>
2020-01-30 18:00:54 +0100

    noe

--- RiverSizes/RiverFinder.cs
+++ RiverSizes/RiverFinder.cs
@@ -28,33 +28,41 @@ namespace Riversizes
             return riverSizes;
         }
 
-        public int RecursiveNeighborCheck(int counter, bool[,] zelleAbgeklappert, int[,] matrix, int i, int j)
+        private int RecursiveNeighborCheck(int counter, bool[,] zelleAbgeklappert, int[,] matrix, int i, int j)
         {
             var localCounter = counter;
 
-            if (matrix.GetUpperBound(1) > i + 1 && zelleAbgeklappert[i + 1, j] && matrix[i + 1, j] != 0 )
+            if (matrix.GetUpperBound(1) > i + 1 && !zelleAbgeklappert[i + 1, j] && matrix[i + 1, j] != 0 )
             {
                 zelleAbgeklappert[i + 1, j] = true;
                 localCounter += RecursiveNeighborCheck(counter, zelleAbgeklappert, matrix, i + 1, j);
             }
+            //if(matrix.GetUpperBound(1) > i + 1)
+            //    zelleAbgeklappert[i + 1, j] = true;
 
             if (matrix.GetUpperBound(0) > j + 1 && !zelleAbgeklappert[i, j + 1] && matrix[i, j + 1] != 0 )
             {
                 zelleAbgeklappert[i, j + 1] = true;
                 localCounter += RecursiveNeighborCheck(counter, zelleAbgeklappert, matrix, i, j +1);
             }
+            //if(matrix.GetUpperBound(0) > j + 1)
+            //    zelleAbgeklappert[i, j + 1] = true;
 
             if (matrix.GetLowerBound(1) <= i - 1 && !zelleAbgeklappert[i - 1, j] && matrix[i - 1, j] != 0 )
             {
                 zelleAbgeklappert[i - 1, j] = true;
                 localCounter += RecursiveNeighborCheck(counter, zelleAbgeklappert, matrix, i - 1, j);
             }
+            //if(matrix.GetLowerBound(1) <= i - 1)
+            //    zelleAbgeklappert[i - 1, j] = true;
 
             if (matrix.GetLowerBound(0) <= j - 1 && !zelleAbgeklappert[i, j - 1] &&  matrix[i, j - 1] != 0)
             {
                 zelleAbgeklappert[i, j - 1]= true;
                 localCounter += RecursiveNeighborCheck(counter, zelleAbgeklappert, matrix, i, j -1);
             }
+            //if(matrix.GetLowerBound(0) <= j - 1)
+            //    zelleAbgeklappert[i, j - 1] = true;
 
             return localCounter;
         }
